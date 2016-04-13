240740b38c24490c53aab8d1cc4f81b79459142a
sam_lai <sam_lai@hiiir.com>
2016-04-13 11:46:33 +0800

    Initial Commit

---
+++ SSLLunchPool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSLLunchPool
+//
+//  Created by sam_lai on 4/13/16.
+//  Copyright © 2016 Sam Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
