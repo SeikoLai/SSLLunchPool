d1b698d2ecc1f45d2cca2e8654cfee72f3fb6af4
sam_lai <sam_lai@hiiir.com>
2016-04-23 23:49:36 +0800

    Add place list controller

---
+++ SSLLunchPool/Controller/SSLPlaceViewController/SSLPlaceViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSLPlaceViewController.h
+//  SSLLunchPool
+//
+//  Created by sam_lai on 4/23/16.
+//  Copyright © 2016 Sam Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSLPlaceViewController : UITableViewController
+
+@end
