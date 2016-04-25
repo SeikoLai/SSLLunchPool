34fbbe0f82c9bdad1a1ee879c0b83844cc11268e
sam_lai <sam_lai@hiiir.com>
2016-04-25 18:32:43 +0800

    add toast

---
+++ SSLLunchPool/View/Toast/SSLToast.h
@@ -0,0 +1,13 @@
+//
+//  SSLToast.h
+//  SSLLunchPool
+//
+//  Created by sam_lai on 4/25/16.
+//  Copyright © 2016 Sam Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSLToast : UIView
+
+@end
---
+++ SSLLunchPool/View/Toast/SSLToast.m
@@ -0,0 +1,21 @@
+//
+//  SSLToast.m
+//  SSLLunchPool
+//
+//  Created by sam_lai on 4/25/16.
+//  Copyright © 2016 Sam Lai. All rights reserved.
+//
+
+#import "SSLToast.h"
+
+@implementation SSLToast
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
